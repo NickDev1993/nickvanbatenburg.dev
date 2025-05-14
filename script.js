af933be04d08a45dbc42f771996ecbf4f37c8d7c
BigDreams <72089074+NickDev1993@users.noreply.github.com>
2025-05-14 11:22:13 +0200

    Trying some changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Dark Mode Toggle
 const toggleButton = document.getElementById('dark-mode-toggle');
+
+// Listen for button click to toggle dark mode
 toggleButton.addEventListener('click', () => {
+    // Toggle the "dark-mode" class on the body element
     document.body.classList.toggle('dark-mode');
 });
